94663d50782a71a6279c143d0d6f939f336f187a
Pitt Pauly <pitt.pauly@gmail.com>
2018-05-13 00:33:06 +0200

    initial models created for facilities app

--- facilities/models.py
+++ facilities/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Location(models.Model):
+    coordinates = models.PointField()
+    address = models.CharField()
+    city = models.CharField()
+    state = models.CharField()
+    country = models.CharField()
+    zip = models.CharField(primary_key=True)
+
+class Facility(models.Model):
+    center = models.CharField()
+    center_search_status = models.CharField(
+        choices=("Public", "Private")
+    )
+    facility = models.CharField()
+    facility_url = models.URLField()
+    occupied = models.DateTimeField()
+    status = models.CharField(
+        choices=('Active', 'Inactive')
+    )
+    url_link = models.URLField()
+    record_date = models.DateTimeField()
+    last_update = models.DateTimeField()
+    contact = models.CharField()
+    phone = models.CharField()
+    location = models.ForeignKey(
+        to=Location,
+        on_delete=models.CASCADE,
+
+    )
