0db6fc40d753be43ec34d7d6a099599675d14527
Pitt Pauly <pitt.pauly@gmail.com>
2018-05-14 15:48:52 +0200

    working setup.sh script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # 1. create venv
 # 2. create DB
 # 3. gather JSON data - try to get data from web API, if fails then use local file
-# 4. load fixture (JSON data)
+# 4. load data ( NASA Facilitites JSON data)
 
 
 # 1. create venv
@@ -20,30 +20,34 @@ export PATH=local_base:$PATH
 # install dependencies in Pipfile.lock
 pipenv install --ignore-pipfile
 
-## activate the venv
-#pipenv shell
+# add symbolic link to site-packages to install this app (ie. adding to the PYTHONPATH)
+ln -s "$(pwd)" "$(pipenv --venv)/lib/python3.6/site-packages/astro_test"
+
 
 # 2. create DB
 # running with sqlite for now
+pipenv run python manage.py migrate
+pipenv run python manage.py createdummyadmin --username admin --email admin@mysite.com --password supersafe111
+
 
 # 3. gather JSON data
-
 # default dataset
 base_path="$( pwd )"
-file_to_load="$base_path/facilities/fixtures/9g7e-7hzz.json"
+file_to_load="$base_path/facilities/data/9g7e-7hzz.json"
 
 # try to get fresh data from endpoint, else use the default dataset
 echo "Attempting to gather fresh data from NASA API..."
-pipenv run coreapi get https://data.nasa.gov/resource/9g7e-7hzz.json > facilities/fixtures/new.json 2> /dev/null
+pipenv run coreapi get https://data.nasa.gov/resource/9g7e-7hzz.json > facilities/data/new.json 2> /dev/null
 
 if [ $? -eq 0 ]; then
     echo "Success!"
-    if [ -s "$base_path/facilities/fixtures/new.json" ] ; then
-        file_to_load="$base_path/facilities/fixtures/new.json"
+    if [ -s "$base_path/facilities/data/new.json" ] ; then
+        file_to_load="$base_path/facilities/data/new.json"
     fi
 else
     echo "Failed to retrieve new dataset, using local data"
 fi
 
+
 # 4. load fixture (this will overwrite any data that's already in the database
-pipenv run python manage.py loaddata --app facilities "$file_to_load"
+pipenv run python manage.py initdb "$file_to_load"
